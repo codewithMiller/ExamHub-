3557cfaa0ec977005f50411b316cf23268aa85d7
Jerry Miller <millerjerrykamsi999@gmail.com>
2026-05-11 15:29:39 +0100

    Run database migrations on application startup

--- ExamHub/api/index.py
+++ ExamHub/api/index.py
@@ -5,3 +5,4 @@ from django.core.management import call_command
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examsite.settings')
 
 app = get_wsgi_application()
+call_command('migrate', '--run-syncdb')
