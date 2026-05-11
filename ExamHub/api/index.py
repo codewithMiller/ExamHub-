398c7da2b5a6b91c82f2aba2dfd0b9207ab2ca2f
Jerry Miller <millerjerrykamsi999@gmail.com>
2026-05-11 14:50:25 +0100

    Update index.py

--- ExamHub/api/index.py
+++ ExamHub/api/index.py
@@ -5,4 +5,3 @@ from django.core.management import call_command
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examsite.settings')
 
 app = get_wsgi_application()
-call_command('migrate', '--run-syncdb')
